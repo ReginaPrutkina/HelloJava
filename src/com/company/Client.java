769aa366238006f1de2c2a84d3219c54dfa2dd15
Regina Prutkina <rprutkina@mail.ru>
2020-06-03 11:45:14 +0500

    Java project test

---
+++ src/com/company/Client.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Client {
+    public int sum(int a, int b){
+        return a+b;
+    }
+    public void sumPrint(int a, int b){
+        System.out.println( a+b);
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Client cl;
+
+        {
+            cl = new Client();
+            cl.sumPrint(12,4);
+        }
+
+    }
+
+}
