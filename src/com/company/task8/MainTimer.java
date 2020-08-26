947c4cc33292fcc0a17e054ae6b6f74a530a9bde
Regina Prutkina <rprutkina@mail.ru>
2020-08-26 21:57:52 +0500

    Задание на потоки Таймер.

--- src/com/company/task8/MainTimer.java
+++ src/com/company/task8/MainTimer.java
@@ -1,17 +1,10 @@
 package com.company.task8;
 
 import java.util.Date;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class MainTimer {
 
     public static void main(String[] args)  {
-        Long countSec = 0L;
-        boolean flag1000 = false;
-        Lock lock = new ReentrantLock();
-        Condition condition = lock.newCondition();
         Date start = new Date();
         LifeTime lifeTime = new LifeTime(start.getTime());
 
@@ -21,13 +14,10 @@ public class MainTimer {
         fiveSecThread.start();
         while (((new Date()).getTime() - start.getTime()) < 30000) {
             try {
-                lock.lock();
-                Thread.sleep(1000);
+               Thread.sleep(1000);
 
         } catch (Exception e) {
                 e.printStackTrace();
-            } finally {
-                lock.unlock();
             }
         }
 
